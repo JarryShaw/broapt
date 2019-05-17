9b17144a5aae8b6e5d8672a222c1061ab1ff86ec
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-05-17 20:38:43 +0800

    Regular update
     * bugfix for docker accessing localhost (tested on macOS, to to Linux)
     * revised Makefiles & bundling system

---
+++ source/init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -aex
+
+# load environs
+if [ -f .env ] ; then
+    source .env
+fi
+
+# get OS
+os=`uname | tr "[[:upper:]]" "[[:lower:]]"`
+
+# start server
+bin/broaptd.${os} \
+    --host="127.0.0.1" \
+    --port="5000" \
+    --docker-compose="./docker-compose.yml" \
+    --dump-path="./dump/" \
+    --logs-path="./logs/" \
+    --api-root="./include/api/" \
+    --api-logs="./logs/api/" \
+    --interval="10" \
+    --max-retry="3"
