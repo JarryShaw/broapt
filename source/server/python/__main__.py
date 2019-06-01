629bcfcfab4461a45df37dfdc90698bce694c39a
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-06-01 22:27:48 +0800

    Bugfix

--- source/server/python/__main__.py
+++ source/server/python/__main__.py
@@ -11,6 +11,30 @@ from const import LOGS_PATH, SERVER_NAME_HOST, SERVER_NAME_PORT
 from daemon import app, manager
 from util import docker_compose
 
+# resolve compatibility issue
+if not hasattr(contextlib, 'nullcontext'):
+    class nullcontext(contextlib.AbstractContextManager):
+        """Context manager that does no additional processing.
+
+        Used as a stand-in for a normal context manager, when a particular
+        block of code is only sometimes used with a normal context manager:
+
+        cm = optional_cm if condition else nullcontext()
+        with cm:
+            # Perform operation, using optional_cm if condition is True
+        """
+
+        def __init__(self, enter_result=None):
+            self.enter_result = enter_result
+
+        def __enter__(self):
+            return self.enter_result
+
+        def __exit__(self, *excinfo):  # pylint: disable=arguments-differ
+            pass
+
+    contextlib.nullcontext = nullcontext
+
 
 def redirect(func):
     @functools.wraps(func)
