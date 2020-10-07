253b6a06dd12942c780672f6792f46e2e0f634f6
Jarry Shaw <jarryshaw@icloud.com>
2020-10-08 01:18:49 +0800

    updated Pipfile

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -92,6 +92,7 @@ html_theme_options = {
     'github_user': 'JarryShaw',
     'github_repo': 'broapt',
     'github_banner': True,
+    'github_type': 'star',
     #'show_related': False,
     #'note_bg': '#FFF59C',
     #'travis_button': True,
